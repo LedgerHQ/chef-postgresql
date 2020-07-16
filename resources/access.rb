003df6d13a1bfdeb1ffd3b500ff899ac4779f264
Hervé Werner <herve.werner@ledger.fr>
2020-07-16 19:26:15 +0200

    INFRA-2057: Allow pg_hba.conf to be configured immediately during runtime

--- resources/access.rb
+++ resources/access.rb
@@ -16,16 +16,37 @@
 # limitations under the License.
 #
 
-property :access_type,   String, default: 'local'
-property :access_db,     String, default: 'all'
-property :access_user,   String, default: 'postgres'
-property :access_method, String, default: 'ident'
-property :cookbook,      String, default: 'postgresql'
-property :source,        String, default: 'pg_hba.conf.erb'
-property :access_addr,   String
-property :comment,       String
+property :access_type,       String, default: 'local'
+property :access_db,         String, default: 'all'
+property :access_user,       String, default: 'postgres'
+property :access_method,     String, default: 'ident'
+property :cookbook,          String, default: 'postgresql'
+property :source,            String, default: 'pg_hba.conf.erb'
+property :access_addr,       String
+property :comment,           String
+property :apply_immediately, [true, false], default: false
 
 action :grant do
+  if new_resource.apply_immediately
+    # Set up pg_hba.conf so that the rule can be immediately enforced
+    # This change is temporary as the resource will then edit pg_hba.conf
+    # as usual at a delayed time with all the rules collected at runtime
+    pg_access =
+      <<~EOH.gsub(/\n/, '')
+        #{new_resource.access_type.ljust(8)}
+        #{new_resource.access_db.ljust(16)}
+        #{new_resource.access_user.ljust(16)}
+        #{(new_resource.access_addr ? new_resource.access_addr : "").ljust(24)}
+        #{new_resource.access_method}
+      EOH
+    append_if_no_line "immediately apply access rule in pg_hba.conf" do
+      path "#{conf_dir}/pg_hba.conf"
+      line pg_access
+      ignore_missing false
+      sensitive false
+    end
+  end
+
   config_resource = new_resource
   with_run_context :root do
     edit_resource(:template, "#{conf_dir}/pg_hba.conf") do |new_resource|
